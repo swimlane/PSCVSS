6e17726855e87d4954e23c08e2eacdbe3e8d42e7
Josh Rickard <45767933+joshswimlane@users.noreply.github.com>
2019-04-18 13:01:19 -0500

    Update PSCVSS.psm1

--- PSCVSS/PSCVSS.psm1
+++ PSCVSS/PSCVSS.psm1
@@ -1,4 +1,4 @@
-using module .\PSCVSS\Class\BaseScore.psm1, .\PSCVSS\Class\EnvironmentalScore.psm1, .\PSCVSS\Class\TemporalScore.psm1
+using module '.\PSCVSS\Class\BaseScore.psm1', '.\PSCVSS\Class\EnvironmentalScore.psm1', '.\PSCVSS\Class\TemporalScore.psm1'
 #Get public and private function definition files.
 $Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -Recurse -ErrorAction SilentlyContinue )
 $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -Recurse -ErrorAction SilentlyContinue )
