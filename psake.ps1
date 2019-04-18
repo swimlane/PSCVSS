81def06c453f1955d39e13f6e73202c6387ee0a2
Josh Rickard <45767933+joshswimlane@users.noreply.github.com>
2019-04-18 14:51:06 -0500

    Update psake.ps1

--- psake.ps1
+++ psake.ps1
@@ -85,5 +85,5 @@ Task Deploy -Depends Build {
         Force = $true
         Recurse = $false # We keep psdeploy artifacts, avoid deploying those : )
     }
-    Invoke-PSDeploy @Verbose @Params
+    Invoke-PSDeploy @Params -Verbose
 }
