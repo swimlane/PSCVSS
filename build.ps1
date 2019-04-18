93ee0d6ddcc83472e2ccbdd1b36af08461498155
Josh Rickard <45767933+joshswimlane@users.noreply.github.com>
2019-04-18 15:21:00 -0500

    Update appveyor

--- build.ps1
+++ build.ps1
@@ -1,56 +1,92 @@
-function Resolve-Module
-{
-    [Cmdletbinding()]
-    param
-    (
-        [Parameter(Mandatory)]
-        [string[]]$Name
-    )
+# Line break for readability in AppVeyor console
+Write-Host -Object ''
 
-    Process
-    {
-        foreach ($ModuleName in $Name)
-        {
-            $Module = Get-Module -Name $ModuleName -ListAvailable
-            Write-Verbose -Message "Resolving Module $($ModuleName)"
-            
-            if ($Module) 
-            {                
-                $Version = $Module | Measure-Object -Property Version -Maximum | Select-Object -ExpandProperty Maximum
-                $GalleryVersion = Find-Module -Name $ModuleName -Repository PSGallery | Measure-Object -Property Version -Maximum | Select-Object -ExpandProperty Maximum
+# Make sure we're using the Master branch and that it's not a pull request
+# Environmental Variables Guide: https://www.appveyor.com/docs/environment-variables/
+if ($env:APPVEYOR_REPO_BRANCH -ne 'master') {
+    Write-Warning -Message "Skipping version increment and publish for branch $env:APPVEYOR_REPO_BRANCH"
+}
+elseif ($env:APPVEYOR_PULL_REQUEST_NUMBER -gt 0) {
+    Write-Warning -Message "Skipping version increment and publish for pull request #$env:APPVEYOR_PULL_REQUEST_NUMBER"
+}
+else {
+    # We're going to add 1 to the revision value since a new commit has been merged to Master
+    # This means that the major / minor / build values will be consistent across GitHub and the Gallery
+    Try {
+        # This is where the module manifest lives
+        $manifestPath = '.\PSCVSS\PSCVSS.psd1'
 
-                if ($Version -lt $GalleryVersion)
-                {
-                    
-                    if ((Get-PSRepository -Name PSGallery).InstallationPolicy -ne 'Trusted') { Set-PSRepository -Name PSGallery -InstallationPolicy Trusted }
-                    
-                    Write-Verbose -Message "$($ModuleName) Installed Version [$($Version.tostring())] is outdated. Installing Gallery Version [$($GalleryVersion.tostring())]"
-                    
-                    Install-Module -Name $ModuleName -Force
-                    Import-Module -Name $ModuleName -Force -RequiredVersion $GalleryVersion
-                }
-                else
-                {
-                    Write-Verbose -Message "Module Installed, Importing $($ModuleName)"
-                    Import-Module -Name $ModuleName -Force -RequiredVersion $Version
-                }
-            }
-            else
-            {
-                Write-Verbose -Message "$($ModuleName) Missing, installing Module"
-                Install-Module -Name $ModuleName -Force
-                Import-Module -Name $ModuleName -Force -RequiredVersion $Version
-            }
+        # Start by importing the manifest to determine the version, then add 1 to the revision
+        $manifest = Test-ModuleManifest -Path $manifestPath
+        [System.Version]$version = $manifest.Version
+        Write-Output "Old Version: $version"
+        [String]$newVersion = New-Object -TypeName System.Version -ArgumentList ($version.Major, $version.Minor, $version.Build, $env:APPVEYOR_BUILD_NUMBER)
+        Write-Output "New Version: $newVersion"
+
+        # Update the manifest with the new version value and fix the weird string replace bug
+        $functionList = ((Get-ChildItem -Path .\CaptureIT\Public).BaseName)
+        $splat = @{
+            'Path'              = $manifestPath
+            'ModuleVersion'     = $newVersion
+            'FunctionsToExport' = $functionList
+            'Copyright'         = "(c) 2018-$( (Get-Date).Year ) Swimlane. All rights reserved."
         }
+        Update-ModuleManifest @splat
+        (Get-Content -Path $manifestPath) -replace 'PSGet_PSCVSS', 'CaptureIT' | Set-Content -Path $manifestPath
+        (Get-Content -Path $manifestPath) -replace 'NewManifest', 'CaptureIT' | Set-Content -Path $manifestPath
+        (Get-Content -Path $manifestPath) -replace 'FunctionsToExport = ', 'FunctionsToExport = @(' | Set-Content -Path $manifestPath -Force
+        (Get-Content -Path $manifestPath) -replace "$($functionList[-1])'", "$($functionList[-1])')" | Set-Content -Path $manifestPath -Force
+    }
+    catch {
+        throw $_
+    }
+
+    # Create new markdown and XML help files
+    Write-Host "Building new function documentation" -ForegroundColor Yellow
+    Import-Module -Name "$PSScriptRoot\PSCVSS" -Force
+    New-MarkdownHelp -Module PSCVSS -OutputFolder '.\docs\' -Force
+    New-ExternalHelp -Path '.\docs\' -OutputPath '.\PSCVSS\en-US\' -Force
+    . .\docs.ps1
+    Write-Host -Object ''
+
+    # Publish the new version to the PowerShell Gallery
+    Try {
+        # Build a splat containing the required details and make sure to Stop for errors which will trigger the catch
+        $PM = @{
+            Path        = '.\PSCVSS'
+            NuGetApiKey = $env:NuGetApiKey
+            ErrorAction = 'Stop'
+            Tags         = 'CVSS', 'PowerShell', 'PowerShell Core', 'Vector', 'Vulnerability'
+            LicenseUri   = 'https://github.com/Swimlane/PSCVSS/blob/master/LICENSE'
+            ProjectUri   = 'https://github.com/Swimlane/PSCVSS'
+            ReleaseNotes = 'Initial release to the PowerShell Gallery'
+        }
+
+        Publish-Module @PM
+        Write-Host "PSCVSS PowerShell Module version $newVersion published to the PowerShell Gallery." -ForegroundColor Cyan
+    }
+    Catch {
+        # Sad panda; it broke
+        Write-Warning "Publishing update $newVersion to the PowerShell Gallery failed."
+        throw $_
+    }
+
+    # Publish the new version back to Master on GitHub
+    Try {
+        # Set up a path to the git.exe cmd, import posh-git to give us control over git, and then push changes to GitHub
+        # Note that "update version" is included in the appveyor.yml file's "skip a build" regex to avoid a loop
+        $env:Path += ";$env:ProgramFiles\Git\cmd"
+        Import-Module posh-git -ErrorAction Stop
+        git checkout master
+        git add --all
+        git status
+        git commit -s -m "Update version to $newVersion"
+        git push origin master
+        Write-Host "PSCVSS PowerShell Module version $newVersion published to GitHub." -ForegroundColor Cyan
+    }
+    Catch {
+        # Sad panda; it broke
+        Write-Warning "Publishing update $newVersion to GitHub failed."
+        throw $_
     }
 }
-
-# Grab nuget bits, install modules, set build variables, start build.
-Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
-
-Resolve-Module Psake, PSDeploy, Pester, BuildHelpers
-
-Set-BuildEnvironment
-
-Invoke-psake .\psake.ps1
-exit ( [int]( -not $psake.build_success ) )
